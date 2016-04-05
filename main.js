28afc7703ba9800594404ea1456f24fa55e7395c
Elsis-Sitati <sitatielsis@gmail.com>
2016-04-05 16:59:49 +0300

    Added jquery for image slider

---
+++ main.js
@@ -0,0 +1,9 @@
+$("#, ").vegas({
+    slides: [
+        { src: "img/download.jpg" },
+        { src: "img/images1.jpg" },
+        { src: "img/slide2.jpg" },
+        { src: "img/slide3.jpg" },
+        { src: "img/images4.jpg" }
+    ]
+});
